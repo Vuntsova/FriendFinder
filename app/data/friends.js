ab6e835ee5c48ade8d7bcd8e5e3eaf5ac1eecf80
vuntsova <vuntsova@aol.com>
2017-05-12 12:01:32 -0400

    allFiles

---
+++ app/data/friends.js
@@ -0,0 +1,81 @@
+/**
+ * Created by webohweb on 5/7/17.
+ */
+var friends = [
+    {
+        name: "Brad Pitt",
+        photo: "http://www.xarj.net/wp-content/uploads/2011/09/Brad-Pitt.jpg",
+        scores: [
+            "5",
+            "4",
+            "1",
+            "5",
+            "6",
+            "2",
+            "5"
+        ]
+    },{
+        name: "Angelina Jolie",
+        photo: "http://img.usmagazine.com/article-leads-vertical-300/1351182578_angelina-jolie-290.jpg",
+        scores: [
+            "3",
+            "4",
+            "3",
+            "2",
+            "6",
+            "5",
+            "5"
+        ]
+    },{
+        name: "Neil Patrick Harris",
+        photo: "http://images.nymag.com/arts/tv/profiles/nph090921_2_250.jpg",
+        scores: [
+            "4",
+            "5",
+            "3",
+            "5",
+            "6",
+            "5",
+            "3"
+        ]
+    },{
+        name: "Cobie Smulders",
+        photo: "https://s-media-cache-ak0.pinimg.com/originals/87/7a/a8/877aa8220e056924b090b3c6463d896c.jpg",
+        scores: [
+            "5",
+            "5",
+            "1",
+            "2",
+            "6",
+            "4",
+            "3"
+        ]
+    },{
+        name: "Donald Trump",
+        photo: "https://pbs.twimg.com/profile_images/614526496112144385/O_p28XM2.jpg",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Barack Obama",
+        photo: "http://img.usmagazine.com/article-leads-vertical-300/1250529817_barack_obama_290x402.jpg",
+        scores: [
+            "5",
+            "2",
+            "3",
+            "3",
+            "5",
+            "5",
+            "5"
+        ]
+    }
+];
+
+module.exports = friends;
